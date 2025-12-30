15b51ce56e44d08c2f2802fe39f99b0b5f1f887c
Fede <fedebare04@gmail.com>
2025-12-30 19:24:00 -0300

    Quartz sync: Dec 30, 2025, 7:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Apuntes de la Carrera Ingeniería Mecatrónica",
+    pageTitle: "Mecatrónica - DG",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
